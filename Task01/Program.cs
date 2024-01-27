333ccd13caddc1a751e6a08799a20664a449b9eb
Yureva Nelli <jurjevanellie@mail.ru>
2024-01-27 14:54:30 +0300

    Metod_2

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -10,3 +10,23 @@ void array(string [] Array)
         Array [i] = Console.ReadLine()!;
     }
 }
+string [] symbol (string [] Array)
+{
+    int n = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if(Array[i].Length <= 3)
+        n++;
+    }
+    string [] rez = new string [n];
+    int j = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if(Array[i].Length <= 3)
+        {
+        rez[j] = Array[i];
+        j++;
+        }
+    }
+    return rez;
+}
