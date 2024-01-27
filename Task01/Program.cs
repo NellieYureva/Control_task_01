014fe2236f9f4d6ebcae543dd2f152ed5d2e8f26
Yureva Nelli <jurjevanellie@mail.ru>
2024-01-27 14:58:32 +0300

    Metod_3

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -30,3 +30,15 @@ string [] symbol (string [] Array)
     }
     return rez;
 }
+void printArray(string [] Array)
+{
+    Console.Write("[");
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write($"‘{Array[i]}’");
+        if (i < Array.Length-1) Console.Write(" ,");
+    }
+    Console.Write("]");
+}
+array(Array);
+printArray (symbol(Array));
