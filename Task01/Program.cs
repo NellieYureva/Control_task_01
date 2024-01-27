4969b1929e435308cd4c89a1c4aeb9d322c7ea11
Yureva Nelli <jurjevanellie@mail.ru>
2024-01-27 14:50:08 +0300

    Metod_1

---
+++ Task01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int k = Convert.ToInt32(Console.ReadLine());
+string [] Array = new string [k];
+void array(string [] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i+1} элемент массива");
+        Array [i] = Console.ReadLine()!;
+    }
+}
